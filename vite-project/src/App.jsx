9c7d98ad5a47b88b5a7e4bd5bb37944b1b0fd45f
Kateryna Moskalenko <katyamoskalenko1996@gmail.com>
2024-10-16 22:42:16 +0200

    add files to HW1

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import viteImage from './assets/vite-image.png'
+
+function App() {
+  
+
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+    <h1>Hello world</h1>
+    <img src={viteImage} alt="vite-image" style={{ width: '600px', height: 'auto' }} />
+
+     </div>
+  )
+}
+
+export default App
